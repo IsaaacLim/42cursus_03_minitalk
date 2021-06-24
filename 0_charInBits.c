e056850abeda93fca39c042b9877633fe2957550
isaaclim <isaaclimjj@yahoo.com>
2021-06-25 03:17:12 +0800

    added notes to charInBits

--- 0_charInBits.c
+++ 0_charInBits.c
@@ -2,6 +2,8 @@
 # include <unistd.h>
 # include <signal.h>
 
+// Creating a Char with Bits
+// a = 97 = 01100001
 void	create_char_with_bits(void)
 {
 	char a;
@@ -18,31 +20,32 @@ void	create_char_with_bits(void)
 	printf("\n");
 }
 
-//https://stackoverflow.com/questions/18327439/printing-binary-representation-of-a-char-in-c
-//can try to send signals with this
+// Printing Bits of a Char
+// https://stackoverflow.com/questions/18327439/printing-binary-representation-of-a-char-in-c
+// Send signals through this method
 void	print_char_in_bits(char a)
 {
-  int i;
-  for (i = 0; i < 8; i++) {
-      printf("%d", !!((a << i) & 0x80)); //print from left to right
+  for (int i = 0; i < 8; i++) {
+      printf("%d", !!((a << i) & 0x80)); //print from left bit to right bit
   }
   printf("\n");
 }
 
+// Simulation of how data is sent through signals
+// Global variables are used to retain data values
 static char c;
 static char size = 7;
-
 void	print_bits_in_char(int bit) //receiving 1/0 only
 {
-	c += ((bit & 1) << size); //stringing left to right (== works too)
+	c += ((bit & 1) << size); //stringing left to right[offset of (0&1) or (1&1)]
 	size--;
 	if (size == -1) //7 to 0 offset must be completed
 	{
 		write(1, &c, 1);
 		if (!c)
 			write(1,"\n", 1);
-		c = 0; //NULL?
-		size = 7;
+		c = 0; //reset Char to NULL
+		size = 7; //reset bit position count
 	}
 }
 
