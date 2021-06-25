ab0c93625e0135889d56e3c011582864cb513465
isaaclim <isaaclimjj@yahoo.com>
2021-06-25 15:10:23 +0800

    created messages.c

---
+++ messages.c
@@ -0,0 +1,15 @@
+#include "minitalk.h"
+
+void ft_completed(int sig)
+{
+	ft_putstr("Message successfully sent & print\n");
+}
+
+void ft_failed(int type)
+{
+	if (type == 0)
+		ft_putstr("run: ./client <PID> \"Message\"\n");
+	if (type == 1)
+		ft_putstr("PID not found, check PID\n");
+	exit(type);
+}
